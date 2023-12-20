9030fdca4ea99cf3439196f56a4b1cd0d33cb14e
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-12-20 16:02:27 +0100

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,43 +3,45 @@
 """ script that reads stdin line by line and computes metrics """
 
 import sys
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+summ = 0
 
 
-def printstdin(date, size):
-    """ print information from stdin """
-    print("File size: {:d}".format(size))
-    for i in sorted(date.keys()):
-        if date[i] != 0:
-            print("{}: {:d}".format(i, date[i]))
+def prn_stats():
+    """
+    Function that print stats about log
+    """
+    global summ
+
+    print('File size: {}'.format(summ))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
 
 
-statusCode = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-              "404": 0, "405": 0, "500": 0}
-
-counter = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if counter != 0 and counter % 10 == 0:
-            printstdin(statusCode, size)
-
-        listStatus = line.split()
-        counter += 1
-
-        try:
-            size += int(listStatus[-1])
-        except:
-            pass
-
-        try:
-            if listStatus[-2] in statusCode:
-                statusCode[listStatus[-2]] += 1
-        except:
-            pass
-    printstdin(statusCode, size)
-
-
-except KeyboardInterrupt:
-    printstdin(statusCode, size)
-    raise
+if __name__ == "__main__":
+    cnt = 0
+    try:
+        """ Iter the standar input """
+        for data in sys.stdin:
+            try:
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """ If there is a lenght """
+                summ += int(fact[-1])
+            except:
+                pass
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                prn_stats()
+                cnt = 0
+    except KeyboardInterrupt:
+        prn_stats()
+        raise
+    else:
+        prn_stats()
