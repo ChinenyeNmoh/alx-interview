da673a70ed2ee1e3e51cf5aae735f68331b75485
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2024-01-16 07:52:41 +0100

    starwars_api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,45 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+let id = process.argv.slice(2);
+let url = `https://swapi.dev/api/films/${id}`;
+
+const func = async () => {
+    try {
+        const body = await new Promise((resolve, reject) => {
+            request(url, (error, response, body) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(body);
+                }
+            });
+        });
+
+        const jsonRes = JSON.parse(body);
+
+        for (let characterUrl of jsonRes.characters) {
+            const body2 = await new Promise((resolve, reject) => {
+                request(characterUrl, (err, res, body) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(body);
+                    }
+                });
+            });
+
+            try {
+                const jres = JSON.parse(body2);
+                console.log(jres.name);
+            } catch (parseError) {
+                console.error('Error parsing JSON:', parseError);
+            }
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    }
+};
+
+func();
